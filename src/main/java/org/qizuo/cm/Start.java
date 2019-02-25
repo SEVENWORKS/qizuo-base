53c2054b3586b768702f8d135146ceacf71fcb8a
fangl <a554657207@126.com>
2019-02-25 10:03:58 +0800

    start启动文件基本配置

--- src/main/java/org/qizuo/cm/Start.java
+++ src/main/java/org/qizuo/cm/Start.java
@@ -1,7 +1,9 @@
 package org.qizuo.cm;
 
+import ch.qos.logback.ext.spring.web.LogbackConfigListener;
 import org.qizuo.cm.interceptor.springmvc.SpringmvcDispatcherServlet;
 import org.qizuo.cm.listener.session.SessionListener;
+import org.springframework.context.ApplicationListener;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.XmlWebApplicationContext;
@@ -20,7 +22,22 @@ import java.util.EnumSet;
 public class Start implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        //字符集过滤
+        //spring启动项(Listener配置)
+        servletContext.setInitParameter("contextConfigLocation","classpath:config/spring/applicationContext.xml");
+        servletContext.addListener(ContextLoaderListener.class);
+
+        //日志打印(Listener配置)
+        servletContext.setInitParameter("logbackConfigLocation","classpath:config/log/logback.xml");
+        servletContext.addListener(LogbackConfigListener.class);
+
+        //listener加载(Listener可以用上述的加载方式，也可以用哦个方式)
+        /*ContextLoaderListener logbackConfigLocation=new ContextLoaderListener();
+        logbackConfigLocation.initWebApplicationContext(servletContext);*/
+
+        //单个用户登录监听
+        //servletContext.addListener(SessionListener.class);
+
+        //字符集过滤(filter方式加载)
         CharacterEncodingFilter characterEncodingFilter=new CharacterEncodingFilter();
         characterEncodingFilter.setEncoding("utf-8");
         FilterRegistration.Dynamic encoding =servletContext.addFilter("characterEncodingFilter",characterEncodingFilter);
@@ -30,37 +47,12 @@ public class Start implements WebApplicationInitializer {
         dispatcherTypes.add(DispatcherType.FORWARD);
         encoding.addMappingForUrlPatterns(dispatcherTypes,true,"/");
 
-        //springMvc启动项
+        //springMvc启动项(servlet方式加载)
         XmlWebApplicationContext appContext = new XmlWebApplicationContext();
         appContext.setConfigLocation("classpath:config/spring/spring-mvc.xml");
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new SpringmvcDispatcherServlet(appContext));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
 
-        //spring启动项
-        servletContext.setInitParameter("contextConfigLocation","classpath:config/spring/applicationContext.xml");
-
-        //日志打印
-        servletContext.setInitParameter("logbackConfigLocation","classpath:config/log/logback.xml");
-
-        //单个用户登录监听
-        servletContext.addListener(SessionListener.class);
-
-        //listener加载
-        ContextLoaderListener logbackConfigLocation=new ContextLoaderListener();
-        logbackConfigLocation.initWebApplicationContext(servletContext);
-
-        //url重写
-        /*UrlRewriteFilter urlRewriteFilter=new UrlRewriteFilter();
-        Hashtable<String,String> ht=new Hashtable<>();
-        ht.put("confPath","classpath:config/urlrewrite/urlrewrite.xml");
-        FilterConfig filterConfig=new RunConfig(servletContext,ht);
-        urlRewriteFilter.init(filterConfig);
-        FilterRegistration.Dynamic urlRewrite =servletContext.addFilter("urlRewriteFilter",urlRewriteFilter);
-        EnumSet<DispatcherType> dispatcherTypes2 = EnumSet
-                .allOf(DispatcherType.class);
-        dispatcherTypes.add(DispatcherType.REQUEST);
-        dispatcherTypes.add(DispatcherType.FORWARD);
-        urlRewrite.addMappingForUrlPatterns(dispatcherTypes2,true,"/");*/
     }
 }
---
+++ src/main/java/org/qizuo/cm/utils/ReflectUtil.java
@@ -0,0 +1,9 @@
+package org.qizuo.cm.utils;
+
+/**
+ * @author: fangl
+ * @description: 反射工具类
+ * @date: 8:51 2019/2/25
+ */
+public class ReflectUtil {
+}
